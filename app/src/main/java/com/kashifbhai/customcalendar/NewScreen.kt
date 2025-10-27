5856fe7ee7684c811250ce4ecf4ac41c37905c10
Abdullah123456788 <148084925+Abdullah123456788@users.noreply.github.com>
2025-10-27 11:17:28 +0500

    New Screen

--- app/src/main/java/com/kashifbhai/customcalendar/NewScreen.kt
+++ app/src/main/java/com/kashifbhai/customcalendar/NewScreen.kt
@@ -1,27 +1,15 @@
 package com.kashifbhai.customcalendar
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PaymentInProgress : AppCompatActivity() {
-
-    private lateinit var okbtn: Button
+class NewScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        setContentView(R.layout.activity_payment_in_progress)
-
-        okbtn = findViewById(R.id.okay_btn)
-        okbtn.setOnClickListener {
-            val intent = Intent(this, NewScreen::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_new_screen)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
