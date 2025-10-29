25be00326768d96174f8776a1baf74b1a0d6fe5d
Abdullah123456788 <148084925+Abdullah123456788@users.noreply.github.com>
2025-10-29 17:01:41 +0500

    New Fragment

---
+++ app/src/main/java/com/kashifbhai/customcalendar/New_Fragment.kt
@@ -0,0 +1,29 @@
+package com.kashifbhai.customcalendar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class New_Fragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_new_, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
